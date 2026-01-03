76201ed93bc2398aeb0d3135723cc5c0b25b0c67
Lochard <110186560+locharp@users.noreply.github.com>
2026-01-03 19:26:00 +0000

    Add solution for reversing string prefix

---
+++ LeetCode/Easy/3794. Reverse String Prefix.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    
+    public
+    String
+    reversePrefix
+    (
+        String s
+        , int k
+    )
+    {
+        return new StringBuilder( s.substring( 0, k ) )
+            .reverse()
+            .toString()
+            + s.substring( k );
+    }
+    
+}
